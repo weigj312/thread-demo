0eec4a8f9724455ecb08d1b5b408c3a7cd09f492
WEI <weigj312@gmail>
2016-12-25 11:40:07 +0800

    test

--- src/main/java/com/wei/lock/lockfair/Main.java
+++ src/main/java/com/wei/lock/lockfair/Main.java
@@ -10,9 +10,10 @@ public class Main {
         for(int i=0;i<10;i++){
             threads[i] = new Thread(new Job(printQueue),"Thread"+i);
         }
+
+
         for(Thread t:threads){
             t.start();
-
             try {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
