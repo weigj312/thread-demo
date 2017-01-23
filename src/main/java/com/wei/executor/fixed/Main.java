27df1a95ecec4e662037bd0808fe48635325fe4b
weiguangjian <weiguangjian@jd.com>
2017-01-23 18:01:21 +0800

    thread pool monitor

--- src/main/java/com/wei/executor/fixed/Main.java
+++ src/main/java/com/wei/executor/fixed/Main.java
@@ -1,5 +1,7 @@
 package com.wei.executor.fixed;
 
+import java.util.concurrent.ThreadPoolExecutor;
+
 /**
  * Created by weiguangjian on 2016/12/26.
  */
@@ -12,5 +14,35 @@ public class Main {
             server.executeTask(task);
         }
         server.endServer();
+        System.out.println("isTerminating:"+server.getExecutors().isTerminating());
+        Thread monitorThread = new Thread(new ExecutorMonitor(server.getExecutors()));
+        monitorThread.start();
+        System.out.println("Main -------------主方法结束---------");
+    }
+
+    static class ExecutorMonitor implements Runnable{
+        private ThreadPoolExecutor executor;
+
+        public ExecutorMonitor(ThreadPoolExecutor executor) {
+            this.executor = executor;
+        }
+
+        @Override
+        public void run() {
+            while (true){
+                    if(executor.isTerminated()){
+                        System.out.println("线程执行结束");
+                        break;
+                    }else{
+                        System.out.println("ExecutorMonitor-----------active task accounts:"+executor.getActiveCount());
+                        System.out.println("ExecutorMonitor-----------completedTaskCount:"+executor.getCompletedTaskCount());
+                    }
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+            }
+        }
     }
 }
--- src/main/java/com/wei/executor/fixed/Server.java
+++ src/main/java/com/wei/executor/fixed/Server.java
@@ -21,9 +21,14 @@ public class Server {
         System.out.printf("Server: Active Count: %d\n",executor.getActiveCount());
         System.out.printf("Server: Completed Tasks:%d\n",executor.getCompletedTaskCount());
         System.out.printf("Server: Task Count: %d\n",executor.getTaskCount());
+
     }
 
     public void endServer(){
         executor.shutdown();
+    }
+
+    public ThreadPoolExecutor getExecutors(){
+        return executor;
     }
 }
